e0076e17111944ff9388706374cbebfb9cc1c9b6
Monster0506 <tjraklovits@gmail.com>
2025-09-28 23:00:13 -0400

    Optimizations to tr

--- cmd/tr.go
+++ cmd/tr.go
@@ -13,7 +13,7 @@ import (
 var trCmd = &cobra.Command{
 	Use:   "tr [SET1] [SET2]",
 	Short: "Translate or delete characters",
-	Args:  cobra.RangeArgs(1, 2), // SET1 for delete, SET1 and SET2 for translate
+	Args:  cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
 		deleteMode, _ := cmd.Flags().GetBool("delete")
 		complement, _ := cmd.Flags().GetBool("complement")
@@ -24,9 +24,18 @@ var trCmd = &cobra.Command{
 			set2 = args[1]
 		}
 
+		// Error if delete mode but two sets provided
 		if deleteMode && len(args) == 2 {
 			fmt.Fprintf(os.Stderr, "tr: extra operand '%s'\n", set2)
 			os.Exit(1)
+		}
+
+		expandedSet1 := expandCharSet(set1)
+		expandedSet2 := expandCharSet(set2)
+
+		set1Map := make(map[rune]bool)
+		for _, r := range expandedSet1 {
+			set1Map[r] = true
 		}
 
 		inputReader := bufio.NewReader(os.Stdin)
@@ -40,66 +49,60 @@ var trCmd = &cobra.Command{
 				os.Exit(1)
 			}
 
-			char := string(r)
-			var processedChar string
-
 			if deleteMode {
 				if complement {
-					if !strings.ContainsRune(expandCharSet(set1), r) {
-						processedChar = char
+					if !set1Map[r] {
+						fmt.Print(string(r))
 					}
 				} else {
-					if !strings.ContainsRune(expandCharSet(set1), r) {
-						processedChar = char
+					if !set1Map[r] {
+						fmt.Print(string(r))
 					}
 				}
-			} else { // Translate mode
-				expandedSet1 := expandCharSet(set1)
-				expandedSet2 := expandCharSet(set2)
-
+			} else {
 				idx := strings.IndexRune(expandedSet1, r)
 				if idx != -1 {
 					if idx < len(expandedSet2) {
-						processedChar = string(expandedSet2[idx])
-					} else { // Handle cases where set2 is shorter than set1
-						if len(expandedSet2) > 0 {
-							processedChar = string(expandedSet2[len(expandedSet2)-1]) // Repeat last char
-						} else {
-							processedChar = "" // Delete if set2 is empty
-						}
+						fmt.Print(string(expandedSet2[idx]))
+					} else if len(expandedSet2) > 0 {
+						fmt.Print(string(expandedSet2[len(expandedSet2)-1]))
+					} else {
+						// If set2 empty, delete char
 					}
 				} else {
-					processedChar = char
+					fmt.Print(string(r))
 				}
 			}
-			fmt.Print(processedChar)
 		}
 	},
 }
 
 func init() {
 	trCmd.Flags().BoolP("delete", "d", false, "delete characters in SET1")
-	trCmd.Flags().BoolP("complement", "c", false, "use complement of SET1") // Common for tr, though not explicitly listed in original example
+	trCmd.Flags().BoolP("complement", "c", false, "use complement of SET1")
 }
 
-// expandCharSet expands character ranges like 'a-z' into a full string of characters.
 func expandCharSet(set string) string {
 	var expanded []rune
-	for i := 0; i < len(set); i++ {
-		if i+2 < len(set) && set[i+1] == '-' {
-			start := rune(set[i])
-			end := rune(set[i+2])
+	runes := []rune(set)
+
+	for i := 0; i < len(runes); i++ {
+		if i+2 < len(runes) && runes[i+1] == '-' {
+			start := runes[i]
+			end := runes[i+2]
+
 			if start > end {
-				// Handle inverse ranges (e.g., 'Z-A') if needed, for now assume ascending
 				start, end = end, start
 			}
+
 			for r := start; r <= end; r++ {
 				expanded = append(expanded, r)
 			}
-			i += 2 // Skip the character after '-'
+			i += 2
 		} else {
-			expanded = append(expanded, rune(set[i]))
+			expanded = append(expanded, runes[i])
 		}
 	}
+
 	return string(expanded)
 }
